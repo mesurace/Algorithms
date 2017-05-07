5c5c4c7f2da99fe2a6a9cb76dab36f9d8cd6d6b4
Suresh Adhikari <mesuraceer@gmail.com>
2017-05-07 15:32:48 -0500

    check wheather a number is palindrome or not

---
+++ PalindromeNumber.java
@@ -0,0 +1,29 @@
+package com.algorithms;
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+
+		System.out.println(isPalindrome(1234321));
+	}
+
+	public static boolean isPalindrome(int n) {
+
+		int temp = n;
+		int reverseNumber = 0;
+
+		while (temp > 0) {
+
+			reverseNumber = reverseNumber * 10 + temp % 10;
+			temp = temp / 10;
+
+		}
+
+		if (n == reverseNumber)
+			return true;
+
+		return false;
+
+	}
+
+}
