aab6f7c19404e1159b5b065a60f3f03d6ee6e4d1
Suresh Adhikari <mesuraceer@gmail.com>
2017-05-23 21:57:09 -0700

    second smallest element from int array

---
+++ SecondSmallestElement.java
@@ -0,0 +1,32 @@
+package com.algorithms;
+
+public class SecondSmallestElement {
+
+	public static void main(String[] args) {
+		System.out.println(secondSmallestElement(new int[]{9, 4, 1, 8, 21, 13, 19}));
+
+	}
+	
+	static int secondSmallestElement(int[] array){
+		
+		int small=array[0];
+		int secondSmall=array[1];
+		
+		if(small>secondSmall){
+			small=array[1];
+			secondSmall=array[0];
+		}
+		for(int i=0;i<array.length;i++){
+			int temp=array[i];
+			if(temp<small){
+				secondSmall=small;
+				small=temp;
+			}else if(temp<secondSmall){
+				secondSmall=temp;
+			}
+		}
+		
+		return secondSmall;
+	}
+
+}
