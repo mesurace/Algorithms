b1cc32c828823ca8e0f781c92e8fa598daa96850
Suresh Adhikari <mesuraceer@gmail.com>
2017-07-04 12:10:51 -0700

    refractoring

--- SecondSmallestElement.java
+++ SecondSmallestElement.java
@@ -3,33 +3,34 @@ package com.algorithms;
 public class SecondSmallestElement {
 
 	public static void main(String[] args) {
-		System.out.println(secondSmallestElement(new int[]{9, 4, 1, 8, 21, 13, 19}));
-
+		try {
+			System.out.println(secondSmallest(new int[]{9, 4, 1, 8, 21, 13, 19}));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	static int secondSmallestElement(int[] array){
-		int small;
-		int secondSmall; 
-		if(array.length<2){
-		     return 0;
-		}
-		small=array[0];
-		secondSmall=array[1]; 
-		
-		if(small>secondSmall){
-			small=array[1];
-			secondSmall=array[0];
-		}
-		for(int i=0;i<array.length;i++){
-			int temp=array[i];
-			if(temp<small){
-				secondSmall=small;
-				small=temp;
-			}else if(temp<secondSmall){
-				secondSmall=temp;
+
+	static int secondSmallest(int[] a) throws Exception {
+
+		if (a.length < 2)
+			throw new Exception();
+		if (a.length == 2)
+			return a[1];
+
+		int small = Integer.MAX_VALUE;
+		int secondSmall = Integer.MAX_VALUE;
+
+		for (int i = 0; i < a.length; i++) {
+
+			if (small > a[i]) {
+				secondSmall = small;
+				small = a[i];
+			} else if (secondSmall > a[i]) {
+				secondSmall = a[i];
 			}
+
 		}
-		
+
 		return secondSmall;
 	}
-
 }
